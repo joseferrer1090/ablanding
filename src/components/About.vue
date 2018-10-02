4e989a5bd054682e21e83817c72146db45406376
Jose Carlos <jcfb90@gmail.com>
2018-10-02 00:42:29 -0500

    Change => about area

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,19 +1,31 @@
 <template>
-    <section class="skill-area">
-        <div class="row">
-         <div class="col-md-6">
-            <div class="container">
-                <div class="row">
-                    <h1>THE LARGE INSPECTION COMPANY SEVERING SOUTH FLORIDA</h1>
+    <!-- ABOUT AREA START HERE -->
+    <section class="about-area" id="about" style="margin: 0">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5" >
+                    <div class="section-text">
+                        <h3 class="text-left">THE LARGEST INSPECTION COMPANY SERVERING SOUTH FLORIDA <span class="color">SINCE 1993</span></h3>
+                        <p class="s-title text-left" style="font-size: 13px;">
+                            BA Engineering comprehensive inspection services include residential, commercial and environmental assessments. 
+                            We are proud to lead the field in home and building inspections in south Florida, and continuously strive to meet the evolving needs of the market. 
+                            Our team has been expertly trained including personal training by John Micali, President of Allied Building Inspection Services, 
+                            and provide a very thorough evaluation of each property with inspection reports in most cases provided the following business day. 
+                            Customer needs are our priority-and that is why Allied is the top choice of realtors, homebuyers and real estate professionals in South Florida, 
+                            with thousands of inspections performed yearly. Call us to see how we can help you!
+                        </p>
+                        <div class="col-md-12 dateInformation" >
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem tempora natus blanditiis odit, eveniet impedit aspernatur corporis distinctio commodi quas eligendi, adipisci nostrum ipsum eius laboriosam, maiores totam laborum. Modi.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-7" style="border: 1px solid red">
+                    <img src="" alt="Mujer con la calculadora">
                 </div>
             </div>
-         </div>
-         <div class="col-md-6">
-           <!-- Imagen de la persona con la calculadora -->
-         </div>
         </div>
     </section>
-    <!-- SKILL AREA END HERE -->
+    <!-- ABOUT AREA END HERE -->
 </template>
 <script>
   export default {
